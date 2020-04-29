02eece6f8f309f2adea9ac5d4f762028c857ab7f
Karen Bagratyan <coconutmkii@gmail.com>
2020-04-29 22:14:45 +0300

    darova nahyi

--- src/kek.java
+++ src/kek.java
@@ -1,6 +1,9 @@
 public class kek {
     public static void main(String[] args) {
         System.out.println();
+        System.out.println("asdasd");
+        System.out.println("asdasdasdasd");
+        System.out.println("asdasdasdasdasdasdasdasdasd");
     }
 }
 
